1d372fec29cb53b00688154d9cd80cb5b163eb95
Eli José Carrasquero [PC] <ielijose@gmail.com>
2015-01-19 19:16:35 -0430

    db

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
+			'database'  => 'mlmfunnel',
 			'username'  => 'forge',
-			'password'  => '',
+			'password'  => 'aIxQq4AdfmvibVD3VeR2',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
