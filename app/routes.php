7d4e9b38a5728723db4db261e8a1e3eda414d28c
Eli José Carrasquero [PC] <ielijose@gmail.com>
2015-01-22 11:24:24 -0430

    postreceive

--- app/routes.php
+++ app/routes.php
@@ -55,3 +55,25 @@ App::missing(function($exception)
 
 /* Funnel */
 Route::get('{user}/{landing}', ['uses' => 'HomeController@landing']);
+
+Route::get('postreceive', function(){
+	try
+	{
+		$payload = json_decode($_REQUEST['payload']);
+	}
+	catch(Exception $e)
+	{
+		exit(0);
+	}
+
+//log the request
+	file_put_contents('/logs/github.txt', print_r($payload, TRUE), FILE_APPEND);
+
+
+	if ($payload->ref === 'refs/heads/master')
+	{
+  // path to your site deployment script
+		exec(public_path().'../postreceive.sh');
+	}
+});
+
---
+++ postreceive.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+composer update
+git pull
