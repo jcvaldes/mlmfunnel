da6f7545d68d00608ff409e376643009a5301f20
Eli José Carrasquero [PC] <ielijose@gmail.com>
2015-01-22 10:53:36 -0430

    QR code y enlace

--- app/views/backend/landings/index.blade.php
+++ app/views/backend/landings/index.blade.php
@@ -2,6 +2,18 @@
 
 @section('css')
 <link rel="stylesheet" href="/assets/plugins/magnific/magnific-popup.css">
+<style>
+    .qrcode {
+      width:160px;
+      height:160px;
+      margin-top:15px;
+    }
+
+    a.url{
+        text-shadow: 0 0 2px #999;
+        font-size:24px;
+    }
+</style>
 @stop
 
 @section('content')
@@ -19,7 +31,7 @@
                     <div class="row">
                         <div class="col-md-12 col-sm-12 col-xs-12 table-responsive table-blue filter-right">
 
-                            <div class="row article">
+                            <div class="row article landing">
                                 <div class="col-md-3">
                                     <div class="thumbnail">
                                         <div class="overlay">
@@ -30,12 +42,18 @@
                                         <img src="/assets/img/landings/landing.jpg" alt="/animal" class="img-responsive">
                                     </div>    
                                 </div>
-                                <div class="col-md-9">
+                                <div class="col-md-6">
                                     <h3><a href="#">Landing page</a></h3>
                                     <div class="search-info">
-                                        <span class="search-date"><i class="fa fa-rocket"></i>Inactiva</span>
+                                        {{--<span class="search-date"><i class="fa fa-rocket"></i>Inactiva</span>--}}
                                     </div>
-                                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia , nisi ut aliquid ex ea commodi consequatur? </p>
+                                    <p><br>
+                                        <a class="url" data-qr="qr-landing" href="{{url()}}/{{Auth::user()->username}}/landing">{{url()}}/{{Auth::user()->username}}/landing</a>
+                                    </p>                                    
+                                </div>
+
+                                <div class="col col-md-3 center">
+                                    <div id="qr-landing" class="qrcode"></div>
                                     <div><a class="btn btn-info f-right m-t-10" href="/dashboard/landing/configure/"><i class="fa fa-gear"></i> Configurar</a></div>
                                 </div>
                             </div>
@@ -49,6 +67,23 @@
     </div>
 </div>
 @stop
+
 @section('javascript')
 <script src="/assets/plugins/magnific/jquery.magnific-popup.min.js"></script>
+<script src="/assets/plugins/qrcode/qrcode.min.js"></script>
+<script>
+    $(document).on("ready", function(){
+        
+        $.each($("a.url"), function(index, el) {
+            var qrcode = new QRCode($(this).data('qr'), {
+                text: $(this).text(),
+                width: 128,
+                height: 128,
+                colorDark : "#000000",
+                colorLight : "#ffffff",
+                correctLevel : QRCode.CorrectLevel.H
+            });
+        });
+    })
+</script>
 @stop
