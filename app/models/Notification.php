f010ad68b6136279555bf50a860103021f95207e
Eli José Carrasquero [Laptop] <ielijose@gmail.com>
2015-02-24 11:22:08 -0430

    reverse test mail phone

--- app/models/Notification.php
+++ app/models/Notification.php
@@ -26,18 +26,18 @@ class Notification extends Model {
                     $prospect = Prospect::find($notification->type_id);
                     $user = User::find($notification->user_id);
 
-                    //if($user->notif_email==1){
+                    if($user->notif_email==1){
                         $data = ['name' => $prospect->name, 'email' => $prospect->email, 'phone' => $prospect->phone];
                         Mail::queue('emails.notify.new-prospect', $data, function($message) use ($user)
                         {
                             $message->from('noreply@mlmfunnel.com', 'MLMfunnel');
                             $message->to($user->email, $user->full_name)->subject('Nuevo prospecto! - MLMfunnel');
                         });
-                   // }
-                    //if($user->notif_phone==1){
+                    }
+                    if($user->notif_phone==1){
                         Clickatell::send("Nuevo Interesado en ".$prospect->type." - Nombre: ".$prospect->name." Email: ".$prospect->email." Tel.: ".$prospect->phone, $user->phone);
                         //Heywire::text($user->phone, "Nuevo Interesado en ".$prospect->type." - Nombre: ".$prospect->name." Email: ".$prospect->email." Tel.: ".$prospect->phone);
-                   // }                    
+                    }                    
                 break;
             }
         });       
