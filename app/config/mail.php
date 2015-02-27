4a733c1af81a0dd69d7301356bd1fe6649d7954a
Eli José Carrasquero [Laptop] <ielijose@gmail.com>
2015-02-27 14:00:23 -0430

    Sendmail

--- app/config/mail.php
+++ app/config/mail.php
@@ -1,6 +1,18 @@
 <?php
-
 return array(
+	'driver' => 'sendmail',
+	'host' => 'smtp.gmail.com',
+	'port' => 465,
+	'from' => array('address' => Setting::key('app_mail')->first()->value, 'name' => Setting::key('app_name')->first()->value),
+	'encryption' => 'ssl',
+
+	'username' => 'mailerservice2015@gmail.com',
+	'password' => '24436525',
+	'sendmail' => '/usr/sbin/sendmail -bs',
+	'pretend' => false,
+);
+
+/* return array(
 	'driver' => 'smtp',
 	'host' => 'smtp.sendgrid.net',
 	'port' => 587,
@@ -8,4 +20,4 @@ return array(
 	'encryption' => 'tls',
 	'username' => 'ielijose',
 	'password' => '2512368eli',
-);
+); */
