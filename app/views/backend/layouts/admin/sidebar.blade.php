f83dde2bc38b52e7a6eea7f3f0e8cdf62bb00a71
Eli José Carrasquero [Laptop] <ielijose@gmail.com>
2015-03-03 00:05:42 -0430

    Settings NavBar

--- app/views/backend/layouts/admin/sidebar.blade.php
+++ app/views/backend/layouts/admin/sidebar.blade.php
@@ -11,6 +11,10 @@
             <li class="<?= Request::is('*profile*') ? 'current' : '' ?>">
                 <a href="/dashboard/profile"><i class="glyph-icon flaticon-account"></i><span class="sidebar-text">Mi perfil</span></a>
             </li> 
+
+            <li class="<?= Request::is('*settings*') ? 'current' : '' ?>">
+                <a href="/dashboard/settings"><i class="fa fa-gear"></i><span class="sidebar-text">Configuración</span></a>
+            </li>             
                         
             <br>
         </ul>
