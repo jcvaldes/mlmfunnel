f63edb3e221d5bfdbc88f0fa2cc9376e98d837b1
Eli José Carrasquero [Laptop] <ielijose@gmail.com>
2015-03-13 20:05:43 -0430

    Server reload

--- app/controllers/Server.php
+++ app/controllers/Server.php
@@ -4,12 +4,16 @@ class Server extends BaseController {
 
 	public function deploy() {
 
-		SSH::into('production')->run(array(
-			'cd /home/dineroysalud/public_html/',
-			'git pull origin master'
-		), function($line){
-			echo nl2br($line.PHP_EOL);
-		});
+		try{
+			SSH::into('production')->run(array(
+				'cd /home/dineroysalud/public_html/',
+				'git pull origin master'
+				), function($line){
+				echo nl2br($line.PHP_EOL);
+			});
+		}catch(Exception $e){
+			echo "<script>location.reload();</script>";
+		}		
 		
 	}
 
