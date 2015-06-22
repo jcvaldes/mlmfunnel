ad6fd0c42a6c4c8496b322a83ad80a1c995582dd
Eli José Carrasquero [Laptop] <ielijose@gmail.com>
2015-06-21 22:40:36 -0430

    lang reminder

--- app/lang/es/reminders.php
+++ app/lang/es/reminders.php
@@ -1,26 +1,26 @@
 <?php
 
-return array(
+return [
 
-	/*
-	|--------------------------------------------------------------------------
-	| Password Reminder Language Lines
-	|--------------------------------------------------------------------------
-	|
-	| The following language lines are the default lines which match reasons
-	| that are given by the password broker for a password update attempt
-	| has failed, such as for an invalid token or invalid new password.
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reminder Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
 
-	"password" => "Passwords must be at least six characters and match the confirmation.",
+    "password" => "Las contraseñas deben contener al menos 6 caracteres y coincidir.",
 
-	"user" => "We can't find a user with that e-mail address.",
+    "user"     => "No podemos encontrar a un usuario con ese correo electrónico.",
 
-	"token" => "This password reset token is invalid.",
+    "token"    => "Este token de recuperación de contraseña es inválido.",
 
-	"sent" => "Password reminder sent!",
+    "sent"     => "¡Recordatorio de contraseña enviado!",
 
-	"reset" => "Password has been reset!",
+    "reset"    => "¡Tu contraseña ha sido restablecida!",
 
-);
+];
